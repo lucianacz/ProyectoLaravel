eaf9ba6051a95133a1c34b2df1e00d80b725996b
Luciana <lucianaczikk@gmail.com>
2020-03-03 02:22:49 -0300

    mensaje

--- public/contact.php
+++ public/contact.php
@@ -1,3 +1,3 @@
 <?php
-  mail('culturasariri@gmail.com', $_POST['name']), $_POST['email'], $_POST['mensaje']);
+  mail('culturasariri@gmail.com', "Contact From - " $_POST['name'], $_POST['email'], $_POST['mensaje'], $_POST['name']);
 ?>
