dfed746205a5277296b0820513f4cfcfc769ad65
Luciana <lucianaczikk@gmail.com>
2020-03-03 02:00:06 -0300

    confirmar enviado

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -35,28 +35,22 @@
 </div>
 
 
-<button class="btn btn-primary" type="submit">Enviar</button>
+<button class="btn btn-primary" onclick="event.preventDefault();confirm();">Enviar</button>
 </form>
 
 <script>
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-'use strict';
-window.addEventListener('load', function() {
-// Fetch all the forms we want to apply custom Bootstrap validation styles to
-var forms = document.getElementsByClassName('needs-validation');
-// Loop over them and prevent submission
-var validation = Array.prototype.filter.call(forms, function(form) {
-  form.addEventListener('submit', function(event) {
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    form.classList.add('was-validated');
-  }, false);
-});
-}, false);
-})();
+
+
+function confirm(event){
+  swal({
+    title: "Perfecto",
+    text: "Ya enviaste tu consulta!",
+    icon: "success",
+    button: "Aww yiss!",
+  });
+}
+
+
 
 
 </script>
