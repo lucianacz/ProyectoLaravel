5bffcacb8e9fd9062a814176544c5e5c0fe97de3
Luciana <lucianaczikk@gmail.com>
2020-03-03 02:46:34 -0300

    cont

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -64,7 +64,7 @@ class MainController extends Controller
 
   public function explora() {
     $fotos = Photo::orderby('photo_id')->get();
-    $usuarios = Usuario::orderby('Nombre')->get();
+    $usuarios = User::orderby('name')->get();
     $logeado = \Auth::user();
     return view ('explora', compact('fotos', 'usuarios', 'logeado'));
   }
