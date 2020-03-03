509336952cc6ab6c803d9f2da8482b4d4e79cb25
Luciana <lucianaczikk@gmail.com>
2020-03-03 02:16:14 -0300

    mensaje

--- public/contact.php
+++ public/contact.php
@@ -1,18 +1,3 @@
 <?php
   mail('culturasariri@gmail.com', $_POST['name'], $_POST['email'], $_POST['mensaje']);
 ?>
-
-<script>
-
-
-function confirm(event){
-  swal({
-  title: "Enviado con exito",
-  icon: "success",
-})
-};
-
-onclick="event.preventDefault();confirm();"
-
-
-</script>
