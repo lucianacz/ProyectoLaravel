aebf85093638a075d42757225410d2b6dbb0fc16
Luciana <lucianaczikk@gmail.com>
2020-03-09 14:15:01 -0300

    reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -2,22 +2,21 @@
 
 @section('main')
 
-<link rel="stylesheet" href="css/estiloscarga.css">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header">{{ __('Cambiar contraseña') }}</div>
 
                 <div class="card-body">
-                  <form method="POST" action="{{ route('password.update') }}"> 
+                  <form method="POST" action="{{ route('password.update') }}">
                         @csrf
 
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
@@ -55,7 +54,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
+                                    {{ __('Cambiar contraseña') }}
                                 </button>
                             </div>
                         </div>
