605355951d7e953dd4ec3ee85cc6451db1c20a54
Luciana <lucianaczikk@gmail.com>
2020-03-09 15:04:11 -0300

    reset

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -27,7 +27,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/email';
 
     /**
      * Create a new controller instance.
