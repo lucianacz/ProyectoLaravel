c153e41fb3b8446ba6e5920c876cbf1dc4089f90
Luciana <lucianaczikk@gmail.com>
2020-03-09 11:38:43 -0300

    camera

--- resources/views/newNote.blade.php
+++ resources/views/newNote.blade.php
@@ -95,7 +95,7 @@
   <div class="col-12" style="padding-top:5px;">
     <label for="foto">Imagen 1</label>
     <input id="files" class="form-control @error('foto') is-invalid @enderror" name="foto" style="display: null;" type="file">
-    <a href="#" class="uk-icon-button  uk-margin-small-right" uk-icon="camera"></a>
+    <a class="uk-icon-button  uk-margin-small-right" uk-icon="camera"></a>
     @error('foto')
         <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
@@ -105,7 +105,7 @@
 
   <div class="col-12" style="padding-top:5px;">
     <label for="foto2">Imagen 2</label>
-    <input id="files" class="form-control @error('foto2') is-invalid @enderror" name="foto2" style="display: null;" type="file" class="uk-icon-button  uk-margin-small-right" uk-icon="camera">
+    <input id="files" class="form-control @error('foto2') is-invalid @enderror uk-icon-button  uk-margin-small-right" name="foto2" style="display: null;" type="file" uk-icon="camera">
     @error('foto2')
         <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
