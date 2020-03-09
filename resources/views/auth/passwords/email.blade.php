5f9cc69db364afb713a9fe5b2cac0744e20dd9d0
Luciana <lucianaczikk@gmail.com>
2020-03-09 14:10:57 -0300

    email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layout')
 
-@section('content')
+@section('main')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
