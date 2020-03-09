4283edf681325a4ef7c6e38781a4d77f652e212a
Luciana <lucianaczikk@gmail.com>
2020-03-09 14:05:27 -0300

    reset password

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -6,29 +6,33 @@ use Illuminate\Http\Request;
 
 class AccountsController extends Controller
 {
-        //You can add validation login here
+
+  public function validatePasswordRequest($email, $token)
+   {
+     //You can add validation login here
       $user = DB::table('users')->where('email', '=', $request->email)
-        ->first();
+          ->first();
       //Check if the user exists
       if (count($user) < 1) {
-        return redirect()->back()->withErrors(['email' => trans('User does not exist')]);
+          return redirect()->back()->withErrors(['email' => trans('User does not exist')]);
       }
 
       //Create Password Reset Token
       DB::table('password_resets')->insert([
-        'email' => $request->email,
-        'token' => str_random(60),
-        'created_at' => Carbon::now()
+          'email' => $request->email,
+          'token' => str_random(60),
+          'created_at' => Carbon::now()
       ]);
       //Get the token just created above
       $tokenData = DB::table('password_resets')
-        ->where('email', $request->email)->first();
+          ->where('email', $request->email)->first();
 
       if ($this->sendResetEmail($request->email, $tokenData->token)) {
-        return redirect()->back()->with('status', trans('A reset link has been sent to your email address.'));
+          return redirect()->back()->with('status', trans('A reset link has been sent to your email address.'));
       } else {
-        return redirect()->back()->withErrors(['error' => trans('A Network Error occurred. Please try again.')]);
+          return redirect()->back()->withErrors(['error' => trans('A Network Error occurred. Please try again.')]);
       }
+   }
 
 
       private function sendResetEmail($email, $token)
@@ -38,37 +42,41 @@ class AccountsController extends Controller
       //Generate, the password reset link. The token generated is embedded in the link
       $link = config('base_url') . 'password/reset/' . $token . '?email=' . urlencode($user->email);
 
-        try {
-        //Here send the link with CURL with an external email API
-            return true;
-        } catch (\Exception $e) {
-            return false;
-        }
+          try {
+          //Here send the link with CURL with an external email API
+              return true;
+          } catch (\Exception $e) {
+              return false;
+          }
       }
+
+
+      
+
       public function resetPassword(Request $request)
-      {
+  {
       //Validate input
       $validator = Validator::make($request->all(), [
-      'email' => 'required|exists:users,email',
-      'password' => 'required|confirmed'
+          'email' => 'required|exists:users,email',
+          'password' => 'required|confirmed'
       ]);
 
       //check if input is valid before moving on
       if ($validator->fails()) {
-      return redirect()->back()->withErrors(['email' => 'Please complete the form']);
+          return redirect()->back()->withErrors(['email' => 'Please complete the form']);
       }
 
       $password = $request->password;
-      // Validate the token
+  // Validate the token
       $tokenData = DB::table('password_resets')
       ->where('token', $request->token)->first();
-      // Redirect the user back to the password reset request form if the token is invalid
+  // Redirect the user back to the password reset request form if the token is invalid
       if (!$tokenData) return view('auth.passwords.email');
 
       $user = User::where('email', $tokenData->email)->first();
-      // Redirect the user back if the email is invalid
+  // Redirect the user back if the email is invalid
       if (!$user) return redirect()->back()->withErrors(['email' => 'Email not found']);
-      //Hash and update the new password
+  //Hash and update the new password
       $user->password = \Hash::make($password);
       $user->update(); //or $user->save();
 
@@ -81,8 +89,11 @@ class AccountsController extends Controller
 
       //Send Email Reset Success Email
       if ($this->sendSuccessEmail($tokenData->email)) {
-      return view('index');
+          return view('index');
       } else {
-      return redirect()->back()->withErrors(['email' => trans('A Network Error occurred. Please try again.')]);
+          return redirect()->back()->withErrors(['email' => trans('A Network Error occurred. Please try again.')]);
       }
+
+}
+
 }
