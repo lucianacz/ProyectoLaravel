249c6c9d28195f6f9c2ff7f46bf52c08116eccfd
Luciana <lucianaczikk@gmail.com>
2020-03-10 00:09:40 -0300

    d

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -11,7 +11,7 @@
 
                 <div class="card-body">
                   <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+                      {{ csrf_field() }}
 
 
                         <input type="hidden" name="token" value="{{ $token }}">
