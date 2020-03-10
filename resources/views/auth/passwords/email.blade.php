c3f06c9e4a725743214dc670e852d2b17e1358e6
Luciana <lucianaczikk@gmail.com>
2020-03-10 00:33:04 -0300

    f

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -11,7 +11,7 @@
 
                 <div class="card-body">
                   <form method="POST" action="{{ url('/reset_password_without_token') }}">
-                      {!! csrf_field() !!}
+                    {{ csrf_field() }}
 
 
                         <input type="hidden" name="token" value="{{ $token }}">
