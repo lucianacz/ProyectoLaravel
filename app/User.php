b6e65f89e0790f23fec692e6e413d7744cc8b00f
Luciana <lucianaczikk@gmail.com>
2020-03-09 22:39:58 -0300

    mail

--- app/User.php
+++ app/User.php
@@ -29,6 +29,10 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public function sendPasswordResetNotification($token)
+    {
+       $this->notify(new ResetPassword($token));
+    }
     /**
      * The attributes that should be cast to native types.
      *
