ae5a271a4803b915994c771a91202036e024212a
Luciana <lucianaczikk@gmail.com>
2020-03-17 20:03:38 -0300

    indec

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,6 +26,30 @@
 </section>
 
 
+<div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
+  <div>
+      <div class="uk-card uk-card-default uk-card-body">
+        <div class="uk-inline">
+            <img src="img/cover.jpg" alt="">
+        </div>
+          <h3 class="uk-card-title">Default</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+  </div>
+  <div>
+      <div class="uk-card uk-card-primary uk-card-body">
+          <h3 class="uk-card-title">Primary</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+  </div>
+  <div>
+      <div class="uk-card uk-card-secondary uk-card-body">
+          <h3 class="uk-card-title">Secondary</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+  </div>
+</div>
+
 
 
 
