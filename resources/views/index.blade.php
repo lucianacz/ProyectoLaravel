d0c55f7b4c48f8c128f3b56429952fb7745a6c94
Luciana <lucianaczikk@gmail.com>
2020-03-17 18:50:25 -0300

    slider

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,28 +4,38 @@
 
 <main>
 <section>
-  <div class="row">
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
     <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="img/indexSlider/1.jpg" class="d-block w-100" alt="...">
+      <div class="item active">
+        <img src="/img/indexSlider/1.jpg" alt="Los Angeles">
       </div>
-      <div class="carousel-item">
-        <img src="img/indexSlider/2.jpg" class="d-block w-100" alt="...">
+
+      <div class="item">
+        <img src="chicago.jpg" alt="Chicago">
       </div>
-      <div class="carousel-item">
-        <img src="img/indexSlider/3.jpg" class="d-block w-100" alt="...">
+
+      <div class="item">
+        <img src="ny.jpg" alt="New York">
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
       <span class="sr-only">Next</span>
     </a>
-  </div>
   </div>
 
 </section>
