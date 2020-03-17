6d4fdb7200cf327dfdcb26791d2eea71317f50dd
Luciana <lucianaczikk@gmail.com>
2020-03-17 18:43:57 -0300

    slider

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,27 @@
 <main>
 
 
-  <div class="principal">
-      <div class="scroller">
-        <a href="#culturas" class="uk-icon-button  uk-margin-small-right" uk-icon="arrow-down"></a>
-      </div>
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/sliderIndex/1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/sliderIndex/2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/sliderIndex/3.jpg" class="d-block w-100" alt="...">
+    </div>
   </div>
-
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 
     <section class="identidad">
