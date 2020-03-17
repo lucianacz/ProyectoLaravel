2b1756e22c5fb9659bfb14d9828d19274016e6e3
Luciana <lucianaczikk@gmail.com>
2020-03-17 19:37:25 -0300

    qsomos

--- resources/views/quienessomos.blade.php
+++ resources/views/quienessomos.blade.php
@@ -10,35 +10,26 @@
 
 <main>
   <section class="qSomos">
-    <div class="col-lg-8 col-md-10 col-10">
-      <h4 style="color:grey; margin-top:30px;">¿QUE SIGNIFICA SARIRI?</h4>
-        <p>
-        Sariri en lengua Aymara significa viajero, es quien lleva mensajes entre los pueblos andinos.
-        Nosotros vemos al “Sariri” como un transmisor, como la fuerza que mantiene la tradición viva de
-        generación en generación.</p>
-        <p>
-        Viaja a través del tiempo y el espacio, trasciende las culturas, es el
-        encargado de mantener viva la memoria que cada pueblo guarda en sus habitantes.
-        Cultura Sariri es un proyecto muy joven, de apenas tres meses.</p>
-      </div>
-
-
 
       <div class="col-lg-8 col-md-10 col-10">
         <h4 style="color:grey;">¿QUIENES SOMOS?</h4>
 
-          <p class="text"> ¡Hola! Somos Julian y Luciana. </p>
-
-          <p> Hace un año decidimos salir a viajar por el mundo y en los meses de nuestra aventura recorrimos muchos países de Asia, algunos de África y otros de Europa. Y ya habíamos tenido la posibilidad de viajar previamente por algunos lugares de América Latina. </p>
-
-          <p> En nuestro viaje vivimos un montón experiencias, pero de lo que más nos llamó la atención es la interacción con las diferentes culturas y las realidades paralelas que vivimos. Salimos del circuito turístico y fuimos en busca de lo poco frecuentado. Interactuar con la gente y sentirte parte de las diferentes culturas, no tiene precio. Conocimos así étnias, tribus y distintas sociedades. Nos entregamos a ser parte de diferentes formas de vida. No siempre es necesario el mismo idioma para comunicarnos, a veces son los códigos gestuales como una sonrisa, unirse a una ceremonia o tan simple como probar la comida local.</p>
-
-          <p> Nos dimos cuenta, que el turismo convencional no tiene esta interacción con el mundo local, y es por eso, en base a nuestras vivencias, decidimos lanzar este nuevo proyecto.</p>
-
-          <p> Desde nuestro espacio divulgamos experiencias por el mundo retratando; ceremonias, vivencias, paisajes, entre otras
-          experiencias.</p>
-
+          <p style="white-space: pre-wrap; justify-content: center;">
+          Somos Julian y Luciana.
+          En 2018 decidimos salir a viajar por el mundo y en los meses de nuestra aventura recorrimos muchos países de Asia, algunos de África y otros de Europa. Ya habíamos tenido la posibilidad de viajar previamente por algunos lugares de América.
+          En nuestros viajes vivimos un montón experiencias. Sin dudas las que más nos marcaron fueron las vivencias en las que pudimos generar una interacción real con las diferentes culturas. Cuando viajamos intentamos salirnos del circuito turístico y buscar la autenticidad de los lugares. De esta forma conocimos étnias, tribus y distintas sociedades. Nos entregamos a ser parte de diferentes formas de vida.
+          Fue al final de nuestro viaje cuando nos dimos cuenta que el turismo convencional no logra generar esta interacción con las culturas locales y muchas veces termina perjudicandolas. Es aquí donde nace Cultura Sariri.
+          </p>
         </div>
+
+
+        <div class="col-lg-8 col-md-10 col-10">
+          <h4 style="color:grey; margin-top:30px;">¿QUE SIGNIFICA SARIRI?</h4>
+            <p style="white-space: pre-wrap; justify-content: center;">
+              Sariri en lengua Aymara significa viajero, es quien lleva mensajes entre los pueblos andinos.
+              Nosotros vemos al “Sariri” como un transmisor, como la fuerza que mantiene la tradición viva de generación en generación.
+              Viaja a través del tiempo y el espacio, trasciende las culturas, es el encargado de mantener viva la memoria que cada pueblo guarda en sus habitantes. </p>
+          </div>
 
 
   </section>
