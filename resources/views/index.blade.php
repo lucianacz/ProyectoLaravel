fddee7e431d1a5eaf4b89db76f6a36635d0e75cb
Luciana <lucianaczikk@gmail.com>
2020-03-17 21:34:22 -0300

    indec

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,27 +24,6 @@
 </div>
 
 
-<section>
-    @foreach ($notas as $nota)
-    <?php if ($nota->usuario->adm == 1) :  ?>
-
-      <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
-        <div class="uk-card-media-left uk-cover-container">
-            <img src="/storage/{{$nota->foto}}" alt="" uk-cover>
-            <canvas width="600" height="400"></canvas>
-        </div>
-        <div>
-            <div class="uk-card-body">
-                <h3 class="uk-card-title">Media Left</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-            </div>
-        </div>
-    </div>
-
-    <?php endif; ?>
-    @endforeach
-</section>
-
 
 
 
