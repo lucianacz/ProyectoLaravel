0ebdcdb5b2356f5316c437740a8320495670a694
Luciana <lucianaczikk@gmail.com>
2020-03-26 15:37:23 -0300

    usuario

--- resources/views/gente.blade.php
+++ resources/views/gente.blade.php
@@ -37,7 +37,7 @@
 
         <div class="cuerpoNota">
           <a href="{{url('nota/'.$nota->id)}}" style="text-transform: uppercase; text-align:center; font-weight:bold; color: black;"> {{$nota->subtitulo}} - <strong>{{$nota->pais}}</strong></p>
-          <p style="text-transform: uppercase; text-align:center; color: grey;"> @ {{$nota->usuario->nombreUsuario}} </p></a>
+          <p class="muestraUsuario" style="text-transform: uppercase; text-align:center; color: grey;"> @ {{$nota->usuario->nombreUsuario}} </p></a>
         </div>
       </article>
       @endforeach
