f200e54bed203591e890bae5491d937b3e9a24d5
Luciana <lucianaczikk@gmail.com>
2020-03-29 16:38:13 -0300

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -78,7 +78,6 @@
   </div>
 </section>
 
-<ul id="cultura.sariri" style="display:flex;"></ul>
 
 
 </main>
@@ -88,23 +87,6 @@
 
 
 
-<script type="text/javascript">
-var token = '1362124742.7b33a8d.6613a3567f0a425f9852055b8ef743b7',
-    container2 = document.getElementById( 'cultura.sariri' ),
-    scrElement2 = document.createElement( 'script' );
-
-window.mishaProcessResult2 = function( response ) {
-	container2.innerHTML = '<div><p><img src="' + response.data.profile_picture + '"></p></div>'
-		+ '<div><h1>' + response.data.username + '</h1>'
-		+ '<p>#' + response.data.id + '</p>'
-		+ '<p>' + response.data.counts.media + ' media ' + response.data.counts.followed_by + ' followers ' + response.data.counts.follows + ' follows</p>'
-		+ '<p><strong>' + response.data.full_name + '</strong> ' + response.data.bio + '<a href="' + response.data.website + '">' + response.data.website + '</a></p></div>';
-}
-
-scrElement2.setAttribute( 'src', 'https://api.instagram.com/v1/users/self?access_token=' + token + '&callback=mishaProcessResult2' );
-document.body.appendChild( scrElement2 );
-
-</script>
 
 <!-- UIkit JS -->
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit.min.js"></script>
