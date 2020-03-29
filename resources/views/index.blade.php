db50346e1d7b2ea6e80c9d74c6b350445fd6341d
Luciana <lucianaczikk@gmail.com>
2020-03-29 15:51:00 -0300

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,8 @@
         <li>
             <img src="img/indexSlider/4.jpg" alt="" uk-cover>
             <div class="uk-position-center uk-position-small uk-text-center">
-             <h2 style="font-size:30px;" uk-slideshow-parallax="x: 100,-100">BIENVENIDOS!</h2>
-             <p uk-slideshow-parallax="x: 200,-200">¡Gracias por sumarte a nuestra comunidad!</p>
+             <h2 style="font-size:80px;" uk-slideshow-parallax="x: 100,-100">BIENVENIDOS!</h2>
+             <p uk-slideshow-parallax="x: 200,-200" style="color:white;">¡Gracias por sumarte a nuestra comunidad!</p>
            </div>
         </li>
         <li>
