fc6f7c2ca1865a257938548f0019090f0b96720e
Luciana <lucianaczikk@gmail.com>
2020-03-31 14:47:12 -0300

    nota

---
+++ app/Http/Controllers/Api/GenteController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Nota;
+use App\User;
+
+class GenteController extends Controller
+{
+  public function  all ($id){
+    $user = User::find($id);
+    if ($user->adm)
+    {
+      $data = Nota::all();
+    } else
+    {
+      $data = Nota::where('user_id', '=', $id )->get();
+    }
+      return response ()->json($data);
+    }
+
+
+  }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::get('/edit/{id}','Api\NoteController@all');
+Route::get('/gente','Api\GenteController@all');
