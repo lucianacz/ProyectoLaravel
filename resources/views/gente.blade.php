4421df942f3f46872da0bce2e4fc05bddb03e1c7
Luciana <lucianaczikk@gmail.com>
2020-03-31 14:45:13 -0300

    nota

--- resources/views/gente.blade.php
+++ resources/views/gente.blade.php
@@ -48,24 +48,6 @@
       $("body").css("background-color","red");
 
       var divGeneral = document.querySelector('.row')
-
-      if (datos.length == 0){
-    //crear el div nuevo
-      var divNuevo = document.createElement('div');
-      //agregar al div que contiene el bootstrap
-      divNuevo.setAttribute('class', 'nota col-lg-3 col-md-4 col-11')
-      divNuevo.innerHTML = `
-        <div class="fotoNota">
-        <h4> No hay notas por editar</h4>
-        <h5> Tambien podes:</h5>
-        <a href="http://culturasariri.com.ar/newNote"> Crea una nueva nota </a>
-        </br>
-        <a href="http://culturasariri.com.ar/gente"> Mira las notas </a>
-        </div>
-      `;
-      divGeneral.append(divNuevo);
-      }
-
       for(nota of datos){
 
         //crear el div nuevo
@@ -77,23 +59,18 @@
         //innerHTML Imprime html
           divNuevo.innerHTML = `
 
-        <div class="fotoNota">
+        <div class="fotoNota" style="width:100%; height:25vh;">
 
-        <a href="http://culturasariri.com.ar/edit/nota/${nota.id}"> <img src="/storage/${nota.foto}" alt="${nota.epigrafe}"></a>
-        <a href="http://culturasariri.com.ar/nota/${nota.id}" class="uk-icon-button  uk-margin-small-right" uk-icon="image"></a>
-        <a href="http://culturasariri.com.ar/edit/nota/${nota.id}" class="uk-icon-button  uk-margin-small-right" uk-icon="pencil"></a>
-        <a onclick="event.preventDefault();confirmDelete(event,this,${nota.id});" class="uk-icon-button  uk-margin-small-right" uk-icon="trash"></a>
-
-        <form id="deleteForm${nota.id}" action="{{ url('http://culturasariri.com.ar/delete/nota/${nota.id}') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
+        <a href="http://culturasariri.com.ar/edit/nota/${nota.id}"> <img  style="width:100%; height:24vh; object-fit: cover;" src="/storage/${nota.foto}" alt="${nota.pais}"></a>
         </div>
 
-        <div class="fotoNota">
+        <div class="cuerpoNota">
+        <a href="http://culturasariri.com.ar/edit/nota/${nota.id}" style="text-transform: uppercase; text-align:center; font-weight:bold; color: black;">
+          ${nota.titulo} - <strong> ${nota.pais} </strong></p></a>
+        <p class="muestraUsuario" style="text-transform: uppercase; text-align:center; color: grey;"> @ ${nota.usuario.NombreUsuario} </p>
+      </div>
           <a href="http://culturasariri.com.ar/edit/nota/${nota.id}" style="text-transform: uppercase;"><p style="text-align:center;">
-            ${nota.titulo}
-            <br>
-            ${nota.pais}, ${nota.region}</p></a>
+          <br></p></a>
         </div>  `;
 
 
@@ -118,23 +95,7 @@
   }
    </script>
 
-    <div class="row">
-      @foreach ($notas as $nota)
-      <article class="nota col-lg-3 col-md-4 col-11">
-        <div class="fotoSameSize" style="width:100%; height:25vh;">
-        <a href="{{url('nota/'.$nota->id)}}"> <img style="width:100%; height:24vh; object-fit: cover;" src="/storage/{{$nota->foto}}" alt="{{$nota->pais}}"></a>
-        </div>
 
-
-
-        <div class="cuerpoNota">
-          <a href="{{url('nota/'.$nota->id)}}" style="text-transform: uppercase; text-align:center; font-weight:bold; color: black;"> {{$nota->subtitulo}} - <strong>{{$nota->pais}}</strong></p></a>
-          <p class="muestraUsuario" style="text-transform: uppercase; text-align:center; color: grey;"> @ {{$nota->usuario->nombreUsuario}} </p>
-        </div>
-      </article>
-      @endforeach
-    </div>
-    </div>
   </section>
   <br>
   <section>
