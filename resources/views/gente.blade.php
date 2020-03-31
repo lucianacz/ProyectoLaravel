04a721016ebec763ef88a644a2df7418a59f505f
Luciana <lucianaczikk@gmail.com>
2020-03-31 13:25:58 -0300

    nota

--- resources/views/gente.blade.php
+++ resources/views/gente.blade.php
@@ -29,8 +29,8 @@
     <div class="row">
       @foreach ($notas as $nota)
       <article class="nota col-lg-3 col-md-4 col-11">
-        <div class="fotoSameSize" style="width:100%; height:20vh;">
-        <a href="{{url('nota/'.$nota->id)}}"> <img style="width:100%; height:20vh;" src="/storage/{{$nota->foto}}" alt="{{$nota->pais}}"></a>
+        <div class="fotoSameSize" style="width:100%; height:28vh;">
+        <a href="{{url('nota/'.$nota->id)}}"> <img style="width:100%; height:28vh;" src="/storage/{{$nota->foto}}" alt="{{$nota->pais}}"></a>
         </div>
 
 
