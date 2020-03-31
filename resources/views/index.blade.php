c5a5bc4db12f15217dfbae7830bf33cba699393b
Luciana <lucianaczikk@gmail.com>
2020-03-31 15:19:53 -0300

    pricipal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,23 +4,8 @@
 
 <main>
 <section>
-  <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="min-height: 300; max-height: 700">
-
-    <ul class="uk-slideshow-items">
-        <li>
-            <img src="img/indexSlider/4.jpg" alt="" uk-cover>
-        </li>
-        <li>
-            <img src="img/indexSlider/5.jpg" alt="" uk-cover>
-        </li>
-        <li>
-            <img src="img/indexSlider/6.jpg" alt="" uk-cover>
-        </li>
-    </ul>
-
-    <a class="uk-slidenav-large uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
-    <a class="uk-slidenav-large uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
-
+  
+<div class="principal">
 </div>
 
 <section class="identidad">
