dff22e4492e52815d4dc9f0fde3b324bca76b328
Luciana <lucianaczikk@gmail.com>
2020-03-31 14:39:16 -0300

    filtro

--- resources/views/gente.blade.php
+++ resources/views/gente.blade.php
@@ -23,7 +23,7 @@
     <h5> SUBI TU NOTA</h5>
     <a href="/newNote" class="uk-icon-button  uk-margin-small-right" uk-icon="plus"></a>
 
-<div class="col-lg-3 col-md-4 col-11">
+<div style="text-align:center;" class="col-lg-3 col-md-4 col-11">
   <form method="post" target="" enctype="multipart/form-data">
     @csrf
 
@@ -31,9 +31,9 @@
        <label class="uk-form-label" for="form-horizontal-select">Filtrar</label>
        <div class="uk-form-controls">
            <select onchange="somefunction($(this).val())" class="uk-select" id="form-horizontal-select">
+               <option value="3">Sin filtros</option>
                <option value="1">Cultura Sariri</option>
                <option value="2">Todxs los viajerxs</option>
-               <option value="3">Limpiar filtros</option>
            </select>
        </div>
    </div>
@@ -46,6 +46,68 @@
   function somefunction(value){
      if(value == 1){
       $("body").css("background-color","red");
+
+      var divGeneral = document.querySelector('.row')
+
+      if (datos.length == 0){
+    //crear el div nuevo
+      var divNuevo = document.createElement('div');
+      //agregar al div que contiene el bootstrap
+      divNuevo.setAttribute('class', 'nota col-lg-3 col-md-4 col-11')
+      divNuevo.innerHTML = `
+        <div class="fotoNota">
+        <h4> No hay notas por editar</h4>
+        <h5> Tambien podes:</h5>
+        <a href="http://culturasariri.com.ar/newNote"> Crea una nueva nota </a>
+        </br>
+        <a href="http://culturasariri.com.ar/gente"> Mira las notas </a>
+        </div>
+      `;
+      divGeneral.append(divNuevo);
+      }
+
+      for(nota of datos){
+
+        //crear el div nuevo
+          var divNuevo = document.createElement('div');
+          //agregar al div que contiene el bootstrap
+          divNuevo.setAttribute('class', 'nota col-lg-3 col-md-4 col-11')
+        //clcocar el texto que seria nombre del genero
+
+        //innerHTML Imprime html
+          divNuevo.innerHTML = `
+
+        <div class="fotoNota">
+
+        <a href="http://culturasariri.com.ar/edit/nota/${nota.id}"> <img src="/storage/${nota.foto}" alt="${nota.epigrafe}"></a>
+        <a href="http://culturasariri.com.ar/nota/${nota.id}" class="uk-icon-button  uk-margin-small-right" uk-icon="image"></a>
+        <a href="http://culturasariri.com.ar/edit/nota/${nota.id}" class="uk-icon-button  uk-margin-small-right" uk-icon="pencil"></a>
+        <a onclick="event.preventDefault();confirmDelete(event,this,${nota.id});" class="uk-icon-button  uk-margin-small-right" uk-icon="trash"></a>
+
+        <form id="deleteForm${nota.id}" action="{{ url('http://culturasariri.com.ar/delete/nota/${nota.id}') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+        </div>
+
+        <div class="fotoNota">
+          <a href="http://culturasariri.com.ar/edit/nota/${nota.id}" style="text-transform: uppercase;"><p style="text-align:center;">
+            ${nota.titulo}
+            <br>
+            ${nota.pais}, ${nota.region}</p></a>
+        </div>  `;
+
+
+          //divNuevo.innerHTML = '<div>' + nota.titulo + '<div>';
+        //meter ese div nuev en el div capturado
+        divGeneral.append(divNuevo);
+
+        //console.log(nota);
+      }
+
+
+
+
+
      }
      if(value == 2){
        $("body").css("background-color","yellow");
@@ -55,9 +117,6 @@
      }
   }
    </script>
-
-    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: push; min-height: 300; max-height: 400">
-      <BR>
 
     <div class="row">
       @foreach ($notas as $nota)
