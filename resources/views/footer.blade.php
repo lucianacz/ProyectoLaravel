e8cdbdfdbca2d4efa7618672a6e2d12b47eda7e1
Luciana <lucianaczikk@gmail.com>
2020-04-01 00:52:27 -0300

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -10,11 +10,7 @@
      <p> © 2020 culturasariri.com.ar </br> Todos los derechos reservados </br><strong> Diseño y programación: lucianaczikk@gmail.com</strong></p>
    </div>
 
-   <script language="JavaScript" type="text/javascript">TrustLogo("https://micuenta.donweb.com/img/sectigo_positive_sm.png", "CL1", "none");</script><a href="https://donweb.com/es-ar/certificados-ssl" id="comodoTL" title="Certificados SSL Argentina">Certificados SSL Argentina</a>
-
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js"></script>
-
-<script type="text/javascript">const tlJsHost = ((window.location.protocol == "https:") ? "https://secure.trust-provider.com/" : "http://www.trustlogo.com/"); document.write(unescape("<script src='" + tlJsHost + "trustlogo/javascript/trustlogo.js' type='text/javascript' %3E%3C/script%3E"));</script>
