31f965c8369e587b0c6b355850144b1e7249f2bc
Luciana <lucianaczikk@gmail.com>
2020-04-02 17:23:37 -0300

    header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -7,8 +7,8 @@
         <ul>
           <li><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a></li>
           <li><a href="/"> Home </a></li>
-          <li><a href="/quienessomos"> Quienes somos </a></li>
           <li><a href="/gente"> Relatos de viajerxs </a></li>
+          <li><a href="/quienessomos"> Quienes somos </a></li>
           <li><a href="/contacto"> Contacto </a></li>
           @guest
           <li><a href="/login"> Login </a></li>
