d95c2bf11a7a1e9b62f363ff494e49ecdd08b873
Luciana <lucianaczikk@gmail.com>
2020-04-17 17:05:53 -0300

    index

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -7,7 +7,7 @@
   </div>
 
   <div class="pie">
-     <p> © 2020 culturasariri.com.ar </br> Todos los derechos reservados </br><strong> Diseño y programación: lucianaczikk@gmail.com</strong></p>
+     <p> © 2020 culturasariri.com.ar </br> Todos los derechos reservados </br>Diseño y programación: <a href="www.behance.net/lucianaczikk"> Luciana Czikk</a></p>
    </div>
 
 </footer>
