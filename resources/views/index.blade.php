a4f06cc2ef72a5f9f58e9c6a7bbc79834f3ef311
Luciana <lucianaczikk@gmail.com>
2020-04-17 15:55:20 -0300

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,7 +27,7 @@
 
 <section style="margin-top: 50px" class="col-12">
   <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider>
-    <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-5@m">
+    <ul class="uk-slider-items uk-child-width-1-1 uk-child-width-1-5@m">
       @foreach ($notas as $nota)
         <li style="margin-right: 1%;">
           <a href="{{url('nota/'.$nota->id)}}">
