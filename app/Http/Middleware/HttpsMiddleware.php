9c45d4321ad3f44c81f85bf98865d1c9cbe7fa87
Luciana <lucianaczikk@gmail.com>
2020-04-25 21:29:53 -0300

    http

--- app/Http/Middleware/HttpsMiddleware.php
+++ app/Http/Middleware/HttpsMiddleware.php
@@ -17,15 +17,10 @@ class HttpsMiddleware
      */
      public function handle($request, Closure $next)
      {
-       if (!app()->environment('local')) {
-    // for Proxies
-    Request::setTrustedProxies([$request->getClientIp()]);
+       if (!$request->secure()) {
+            return redirect()->secure($request->getRequestUri());
+        }
 
-    if (!$request->isSecure()) {
-        return redirect()->secure($request->getRequestUri());
-    }
-}
-
-return $next($request);
+        return $next($request);
      }
  }
