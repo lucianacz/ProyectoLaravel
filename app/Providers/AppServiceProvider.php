a7c355c2b52a63cb38e12c65e4f222dddb20aa95
Luciana <lucianaczikk@gmail.com>
2020-04-25 21:16:33 -0300

    http

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,14 +6,11 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+
 
      public function boot()
      {
+       \URL::forceScheme('https');
        //URL::forceScheme('https');
        $this->app['request']->server->set('HTTPS','on');
        //$this->app['request']->server->set('https', 'on');
