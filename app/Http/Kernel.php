5be562c54ecfc1f56f1d35ae31cf5e97c55a24aa
Luciana <lucianaczikk@gmail.com>
2020-04-25 21:02:48 -0300

    inde

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,10 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\HttpsMiddleware::class,
     ];
+
+
 
     /**
      * The application's route middleware groups.
@@ -50,6 +53,7 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
+
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
