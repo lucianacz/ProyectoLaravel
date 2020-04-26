961caae22339058d141adef665c54c3528e22714
Luciana <lucianaczikk@gmail.com>
2020-04-26 19:54:26 -0300

    viajar

--- resources/views/viajar.blade.php
+++ resources/views/viajar.blade.php
@@ -11,20 +11,17 @@
 <main>
   <section class="qSomos">
 
-    <div class="col-lg-9 col-md-10 col-11">
+    <div class="col-lg-9 col-md-10 col-12">
       <h4 style="color:grey; margin-top:20px;">“Nunca dudes en irte lejos, más allá de todos los mares, de todas las fronteras, de todos los países, de todas las creencias”.
 </h4>
         <p style="white-space: pre-wrap; justify-content:flex-start;">
-
-
-
           Viajar nos transforma.
           El viaje nos incomoda, nos desafía e interpela sacándonos de nuestra “zona de confort” para llevarnos a un estado tanto físico como mental elevado. Aqui nuestros sentidos se potencian, cada nuevo estímulo cambiará para siempre nuestra forma de percibir el mundo. Cada nuevo sabor, olor y color nos hacen reinterpretar lo que antes percibimos como delicioso o repugnante, como bello u horrible. Es así cómo nos nutrimos, crecemos y nos expandimos incorporando lo vivido.
           Hay tantas formas de viajar como viajeros, pero todos compartimos esas ansias por conocer, por explorar y por trascender.
           Muchxs de nosotrxs idealizamos viajes románticos como los de Marco Polo, Charles Darwin o Heinrich Harrer, entre otros. A lo mejor nuestros viajes no vayan a ser tan míticos pero no por eso dejan de ser épicos. ¿O acaso nunca sintieron ese vértigo que nos invade cuando estamos a punto de partir hacia una nueva aventura? O ese sentimiento extraño cuando nos damos cuenta que estamos viviendo algo imposible de transmitir con palabras… viajar es vivir de forma plena, es disfrutar el presente y abrazarlo.
 
-          Queremos darles la bienvenida a este nuevo espacio; Creado con la intención de formar una comunidad de viajerxs, en la cual entre todxs podamos compartir experiencias y sabiduría. En esta era de sobreinformación no hay nada mejor que leer relatos de viajeros reales. ¿te sumas? ( llamado a la accion)
-
+          Queremos darles la bienvenida a este nuevo espacio; Creado con la intención de formar una comunidad de viajerxs, en la cual entre todxs podamos compartir experiencias y sabiduría. En esta era de sobreinformación no hay nada mejor que leer relatos de viajeros reales.
+          ¿Te sumas?
          </p>
       </div>
 
