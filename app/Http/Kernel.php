56ce1422438374b15875fbcfcaea066b72544d5e
Luciana <lucianaczikk@gmail.com>
2020-04-25 21:31:29 -0300

    index

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\HttpsMiddleware::class,
+        'HttpsMiddleware' => \App\Http\Middleware\HttpsMiddleware::class,
     ];
 
 
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
 
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'HttpsMiddleware' => \App\Http\Middleware\HttpsMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
