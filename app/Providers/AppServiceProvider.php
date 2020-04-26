ca8fcd0b3072a86a8feb7b8ecbe1c9ac32f763ac
Luciana <lucianaczikk@gmail.com>
2020-04-25 21:11:01 -0300

    index

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      forceScheme('https');
+      //URL::forceScheme('https');
+      $this->app['request']->server->set('HTTPS', 'on');
     }
 }
