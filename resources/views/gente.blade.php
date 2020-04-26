51e8b0df57425590bacf8278fe0512cefd464d09
Luciana <lucianaczikk@gmail.com>
2020-04-26 20:52:44 -0300

    index

--- resources/views/gente.blade.php
+++ resources/views/gente.blade.php
@@ -23,8 +23,8 @@
     </form>*/ ?>
     <section class="identidad">
       <div class="col-lg-8 col-md-12 col-12" id="identidad">
-        <p><a href="/viajar">Queremos darles la bienvenida a este nuevo espacio; Creado con la intención de formar una comunidad de viajerxs, en la cual entre todxs podamos compartir experiencias y sabiduría.
-          <span style="color:grey;">¿Te sumas?</span></a></p>
+        <p style="color:grey;"><a href="/viajar">Queremos darles la bienvenida a este nuevo espacio; Creado con la intención de formar una comunidad de viajerxs, en la cual entre todxs podamos compartir experiencias y sabiduría.
+      ¿Te sumas?</a></p>
 
       </div>
     </section>
