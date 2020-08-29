62ed5144a9dab2ccc9d5ede32085ee0dbad4c7ae
Luciana <lucianaczikk@gmail.com>
2020-08-29 15:34:07 -0300

    Update etiopia.blade.php

--- resources/views/etiopia.blade.php
+++ resources/views/etiopia.blade.php
@@ -5,7 +5,7 @@
 
 <main>
 <section>
-<div class="uk-height-large uk-background-cover uk-light uk-flex uk-flex-top" uk-parallax="bgy: -200" style="background-image: url('images/dark.jpg');">
+<div class="uk-height-large uk-background-cover uk-light uk-flex uk-flex-top" uk-parallax="bgy: -200" style="background-image: url('img/slider1.jpg');">
 
     <h1 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical" uk-parallax="y: 100,0">Headline</h1>
 
