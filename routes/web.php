eb0e56f42995c16a1c5ed08f290358599e49276e
Luciana <lucianaczikk@gmail.com>
2020-10-13 11:00:09 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/gente','MainController@gente');
 Route::get('/viajar','MainController@viajar');
 
 Route::get('/etiopia','MainController@etiopia');
-Route::get('/etiopia','MainController@india');
+Route::get('/india','MainController@india');
 
 Route::get('/quienessomos', 'MainController@qs');
 Route::get('/login', 'MainController@login');
