a61a2f901b7315ade33d4ade577180a3673cbb4c
Luciana <lucianaczikk@gmail.com>
2020-10-20 09:09:19 -0300

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,7 +9,8 @@
           <li><a href="/"> Home </a></li>
           <li><a href="/gente"> Relatos de viaje </a></li>
           <li><a href="/quienessomos"> Quienes somos </a></li>
-          <li><a href="/etiopia"> Etiopia 2021 </a></li>
+          <li><a href="/etiopia"> Viaje a Etiopia 2021 </a></li>
+          <li><a href="/india"> Viaje a India 2021 </a></li>
           <li><a href="/contacto"> Contacto </a></li>
           <!--
           @guest
